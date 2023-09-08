0313529d35c667a98894afa445dd00a7ec706246
Volkov Dmitriy <vdy0555@gmail.com>
2023-09-08 22:15:25 +0700

    playing with colors, information blocks layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
+	darkMode: "media",
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+		},
+		extend: {
+			colors: {
+				"ebony-clay": {
+					50: "#f5f7fa",
+					100: "#ebeff3",
+					200: "#d2dbe5",
+					300: "#aabccf",
+					400: "#7d99b3",
+					500: "#5c7c9b",
+					600: "#486381",
+					700: "#3b5169",
+					800: "#344558",
+					900: "#2f3c4b",
+					950: "#202833",
+				},
+			},
+		},
 	},
 	plugins: [],
 }
